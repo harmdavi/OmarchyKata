9956e95cb5666a858fbaa53eab7a6a03af77f087
David <David>
2025-12-08 21:17:58 -0700

    intial commit

---
+++ appInstRm/installFreeCad.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+sudo pacman -S freecad --noconfirm --needed
---
+++ appInstRm/kataApp.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# TODO make a full update for yay and pacman here before
+# installing the rest of the applications.
+
+
+# Installing applications:
+
+. ./installFreeCad.sh
+. ./installGhostty.sh
+. ./installInkscape.sh
+. ./installKeepassxc.sh
+. ./installStow.sh
+. ./installYazi.sh
+. ./installYtdlp.sh
+
