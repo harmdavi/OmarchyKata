439d9820f791fb72b39d8bb55ea662e92d44f09f
David Harmon <harmdavi@isu.edu>
2026-01-06 16:30:49 -0700

    added in my mouse configuration

--- dotFiles/katadot.sh
+++ dotFiles/katadot.sh
@@ -17,11 +17,28 @@ echo "Removing old config files"
 rm -rf ~/.config/nvim
 rm -rf ~/.local/share/omarchy/default/xcompose
 rm -rf ~/.local/share/omarchy/default/hypr/bindings/tiling-v2.conf
+rm -rf ~/.config/hypr/input.conf 
 #next one that needst to be removed
 
 cd "$REPO_NAME"
 
-stow -t ~ nvim
-stow -t ~ commands
+stow -t ~ custConf 
+stow -t ~ custLocal 
 
- omarchy-restart-xcompose
+while true; do
+    read -rp "A reboot is required to apply changes. Reboot now? (y/n): " answer
+    case "${answer,,}" in
+        y|yes)
+            echo "Rebooting..."
+            reboot
+            break
+            ;;
+        n|no)
+            echo "Reboot canceled. Please reboot later to apply changes."
+            break
+            ;;
+        *)
+            echo "Please answer y or n."
+            ;;
+    esac
+done
